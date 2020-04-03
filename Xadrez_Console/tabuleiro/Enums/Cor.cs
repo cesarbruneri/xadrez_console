f33f2853d59e968ffeba85ef295c6bc7899f47a7
Cesar Bruneri <ccirillobruneri@gmail.com>
2020-04-03 10:41:07 -0300

    Correção do namespace "Tabuleiro" p/ "tabuleiro" Começando a implmentar o tabuleiro

---
+++ Xadrez_Console/tabuleiro/Enums/Cor.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace tabuleiro.Enums
+{
+    enum Cor
+    {
+        Branca,
+        Preta,
+        Amarela,
+        Azul,
+        Vermelha,
+        Verde,
+        Laranja
+    }
+}
